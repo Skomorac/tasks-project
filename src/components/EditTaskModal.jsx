19577fb6021fc09cdf241bf112e29eca6c3c4c4f
Skomorac <admir.skomorac@outlook.com>
2024-06-03 13:52:35 +0200

    styling of edit modal

--- src/components/EditTaskModal.jsx
+++ src/components/EditTaskModal.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Modal, Button, Form } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import PropTypes from "prop-types";
+import "../styles/EditTaskModal.css";
 
 const EditTaskModal = ({
   show,
@@ -38,14 +39,11 @@ const EditTaskModal = ({
           </Form.Group>
         </Form>
       </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
-          {t("close")}
-        </Button>
+      <Modal.Footer className="modal-footer-buttons-container">
         <Button variant="danger" onClick={handleDelete}>
           {t("delete_task")}
         </Button>
-        <Button variant="primary" onClick={handleSave}>
+        <Button variant="success" onClick={handleSave}>
           {t("save_changes")}
         </Button>
       </Modal.Footer>
